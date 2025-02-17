1fcc7c1b21855792356afc5b4e1908957ead1977
The Black Wizard <chuckydu24@gmail.com>
2025-02-17 21:38:15 +0100

    feat(Set-Junction): add nvim junction and test if item already exist

--- Set-Junctions.ps1
+++ Set-Junctions.ps1
@@ -1,3 +1,9 @@
 $scriptRoot = Split-Path $Script:MyInvocation.MyCommand.Path
 
-New-Item -Path "$($env:APPDATA)\alacritty" -ItemType Junction -Value "$scriptRoot\alacritty"
+If (-not (Test-Path "$env:APPDATA\alacritty")) {
+  New-Item -Path "$($env:APPDATA)\alacritty" -ItemType Junction -Value "$scriptRoot\alacritty"
+}
+
+If (-not (Test-Path "$env:LOCALAPPDATA\nvim")) {
+  New-Item -Path "$($env:LOCALAPPDATA)\nvim" -ItemType Junction -Value "$scriptRoot\nvim"
+}
